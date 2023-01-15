66dfcbddb9529186b387bf38acdf3791ecfcf481
Sarc4sticNerd <jplam904@gmail.com>
2023-01-14 23:57:49 -0700

    Fixed display

--- notes.js
+++ notes.js
@@ -14,11 +14,30 @@ class list {
         this.canDrink = false;
         this.cost = 300;
         this.time = 0;
+        this.age=0;
     }
 }
 var doug1 = new list("Colton", "Krupp", "2005-10-05", "Phoenix", "Atlanta", "2023-01-13", "2023-01-16","1","fish", "leg ", "1001")
 var doug2 = new list("Colt", "Kru", "2006-10-05", "Phoenix", "Charleston", "2024-01-13", "2024-01-16","3","chicken", "leg hp ss ", "1002")
 var doug3 = new list("C", "K", "2007-10-05", "Phoenix", "D.C.", "2022-01-13", "2022-01-16","2","vegetarian", "leg win ", "1003")
+doug1.time = (Date.parse(doug1.rD) - Date.parse(doug1.dD))/(24*60*60*1000)
+doug1.age = (Date.now() - Date.parse(doug1.dob))/(24*60*60*1000*365)
+let extraArray = doug1.extras.split(" ");
+if(extraArray.length >= 1) extraArray.pop()
+doug1.cost += Number(doug1.bags*20)+ Number(extraArray.length*10)
+
+doug2.time = (Date.parse(doug2.rD) - Date.parse(doug2.dD))/(24*60*60*1000)
+doug2.age = (Date.now() - Date.parse(doug2.dob))/(24*60*60*1000*365)
+extraArray = doug2.extras.split(" ");
+if(extraArray.length >= 1) extraArray.pop()
+doug2.cost += Number(doug2.bags*20)+ Number(extraArray.length*10)
+
+doug3.time = (Date.parse(doug3.rD) - Date.parse(doug3.dD))/(24*60*60*1000)
+doug3.age = (Date.now() - Date.parse(doug3.dob))/(24*60*60*1000*365)
+extraArray = doug3.extras.split(" ");
+if(extraArray.length >= 1) extraArray.pop()
+doug3.cost += Number(doug3.bags*20)+ Number(extraArray.length*10)
+
 var arr = []
 var ids =["firstName", "lastName", "dob", "dC","aC","dD","rD","bags"]
 var arr2 =["firstNameInfo", "lastNameInfo", "dobInfo", "dCInfo","aCInfo","dDInfo","rDInfo","bagsInfo"]
@@ -52,11 +71,12 @@ function addToList() {
         let checkMap = ids.map(x =>{return document.getElementById(x).value})
         let temp = new list(checkMap[0],checkMap[1],checkMap[2],checkMap[3],checkMap[4],checkMap[5],checkMap[6],checkMap[7],meal,extra,count);
         count++;
-        if((Date.now() - Date.parse(temp.dob)) >= (24*60*60*1000)) temp.canDrink = true
+        if((Date.now() - Date.parse(temp.dob)) >= (21*365*24*60*60*1000)) temp.canDrink = true
         let extraArray = temp.extras.split(" ");
         if(extraArray.length >= 1) extraArray.pop()
         console.log(extraArray)
         temp.time = (Date.parse(temp.rD) - Date.parse(temp.dD))/(24*60*60*1000)
+        temp.age = (Date.now() - Date.parse(temp.dob))/(24*60*60*1000*365)
         temp.cost += Number(temp.bags*20)+ Number(extraArray.length*10)
         arr.push(temp);
         for(let i = 0;i<ids.length;i++) {document.getElementById(ids[i]).value = "";}
@@ -80,12 +100,14 @@ function display(display){
     }
     document.getElementById("mealInfo").value = arr[display].food
     document.getElementById("extraInfo").value = arr[display].extras
+    document.getElementById("age").value = arr[display].age
+    document.getElementById("trip").value = arr[display].time
+    document.getElementById("cost").value = arr[display].cost
     for(let i = 0;i<arr.length;i++)
     {
         if(document.getElementById("firstNameInfo").value == arr[i].firstName && document.getElementById("lastNameInfo").value == arr[i].lastName) 
         {
             ID = i
-            console.log(ID)
         }
     }
 }
@@ -101,6 +123,9 @@ function updateT(){
     }
     arr[ID].food = document.getElementById("mealInfo").value
     arr[ID].extras = document.getElementById("extraInfo").value
+    arr[ID].age = document.getElementById("age").value
+    arr[ID].time = document.getElementById("trip").value
+    arr[ID].cost = document.getElementById("cost").value
 }
 
 function print() {
